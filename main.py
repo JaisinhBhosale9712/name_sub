08b835e9d3de7a6c4d6230bec4431ab0f87ba867
JaisinhBhosale9712 <71847281+JaisinhBhosale9712@users.noreply.github.com>
2022-02-07 11:10:02 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+const [count, setCount] = useState("temp");
+function Hello(props) {
+  return <h1>Hello World!</h1>;;;
+}
+ReactDOM.render(<Hello />, document.getElementById('root'));
